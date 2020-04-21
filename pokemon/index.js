486f8dd3a578ec1cae492e04465382f347051de6
September Utley <september.utley@gmail.com>
2020-04-21 14:42:40 -0600

    improved getAPIData function

--- pokemon/index.js
+++ pokemon/index.js
@@ -1,5 +1,6 @@
 let pokeContainer = document.querySelector('.pokeContainer')
-function getPokeData(url) {
+
+/* function getPokeData(url) {
     fetch(url).then(function (response) {
         response.json().then(function (pokemon) {
             console.log(pokemon.results)
@@ -8,13 +9,35 @@ function getPokeData(url) {
     })
 }
 
-getPokeData("https://pokeapi.co/api/v2/pokemon")
-
+getPokeData("https://pokeapi.co/api/v2/pokemon")*/
 // https://pokeapi.co/api/v2/pokemon/1/
 
+async function getAPIData(url) {
+    try {
+        const response = await fetch(url)
+        const data = response.json()
+        return data
+    }
+    catch (error) {
+        console.error(error)
+    }
+}
 
-function populatePokeCards(pokeArray) {
-    pokeArray.forEach(pokemon => {
+getAPIData('https://pokeapi.co/api/v2/pokemon/?&limit=25').then(
+    (data) => {
+        for (const pokemon of data.results){
+            getAPIData(pokemon.url).then(
+                (pokeData) => {
+                    populatePokeCard(pokeData)
+                }
+            )
+        }
+        
+    }
+)
+
+
+function populatePokeCard(singlePokemon) {
         let pokeScene = document.createElement('div')
         pokeScene.className = 'scene'
         let pokeCard = document.createElement('div')
@@ -24,7 +47,7 @@ function populatePokeCards(pokeArray) {
         })
         let pokeFront = document.createElement('div')
         pokeFront.className = 'card__face card__face--front'
-        pokeFront.textContent = 'Front'
+        pokeFront.textContent = singlePokemon.name
         let pokeBack = document.createElement('div')
         pokeBack.className = 'card__face card__face--back'
         pokeBack.textContent = "Back"
@@ -33,7 +56,6 @@ function populatePokeCards(pokeArray) {
         pokeCard.appendChild(pokeBack)
         pokeScene.appendChild(pokeCard)
         pokeContainer.appendChild(pokeScene)
-    })
 }
 
 /* <div class="scene">
