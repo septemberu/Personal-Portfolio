856844e1cc70c613c67d39c1381c7cac50f5fbcb
September Utley <september.utley@gmail.com>
2020-04-21 14:22:36 -0600

    added event listener to pokeCard

--- pokemon/index.js
+++ pokemon/index.js
@@ -13,23 +13,26 @@ getPokeData("https://pokeapi.co/api/v2/pokemon")
 // https://pokeapi.co/api/v2/pokemon/1/
 
 
-function populatePokeCards (pokeArray) {
+function populatePokeCards(pokeArray) {
     pokeArray.forEach(pokemon => {
-    let pokeScene = document.createElement('div')
-    pokeScene.className = 'scene'
-    let pokeCard = document.createElement('div')
-    pokeCard.className = 'card'
-    let pokeFront = document.createElement('div')
-    pokeFront.className = 'card__face card__face--front'
-    pokeFront.textContent = 'Front'
-    let pokeBack = document.createElement('div')
-    pokeBack.className = 'card__face card__face--back'
-    pokeBack.textContent = "Back"
+        let pokeScene = document.createElement('div')
+        pokeScene.className = 'scene'
+        let pokeCard = document.createElement('div')
+        pokeCard.className = 'card'
+        pokeCard.addEventListener('click', function () {
+            pokeCard.classList.toggle('is-flipped');
+        })
+        let pokeFront = document.createElement('div')
+        pokeFront.className = 'card__face card__face--front'
+        pokeFront.textContent = 'Front'
+        let pokeBack = document.createElement('div')
+        pokeBack.className = 'card__face card__face--back'
+        pokeBack.textContent = "Back"
 
-    pokeContent.appendChild(pokeFront)
-    pokeContent.appendChild(pokeBack)
-    pokeCard.appendChild(pokeContent)
-    pokeContainer.appendChild(pokeCard)
+        pokeCard.appendChild(pokeFront)
+        pokeCard.appendChild(pokeBack)
+        pokeScene.appendChild(pokeCard)
+        pokeContainer.appendChild(pokeScene)
     })
 }
 
@@ -39,4 +42,11 @@ function populatePokeCards (pokeArray) {
     <div class="card__face card__face--back"><div><p>Hi,I'm here on the back</p></div></div>
   </div>
 </div>
-<p>Click card to flip.</p>*/
+<p>Click card to flip.</p>
+
+
+var card = document.querySelector('.card');
+card.addEventListener( 'click', function() {
+  card.classList.toggle('is-flipped');
+});
+*/
